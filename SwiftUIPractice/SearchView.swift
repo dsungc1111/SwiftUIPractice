c78ec08a75de6aa1adc8f69d1f8502f331a1f1b0
dsungc1111 <eotjd9808@naver.com>
2024-09-03 20:58:44 +0900

    - 검색 기능, 수업시간의 업비트 api 메서드 활용하여 적용

--- SwiftUIPractice/SearchView.swift
+++ SwiftUIPractice/SearchView.swift
@@ -15,37 +15,54 @@ struct Company {
 
 struct SearchView: View {
     
-    private var data = [
     
-        Company(name: "Amazon", priority: 0),
-        Company(name: "Apple", priority: 1),
-        Company(name: "Google", priority: 2),
-        Company(name: "Tesla", priority: 3),
-        Company(name: "X", priority: 4),
-        Company(name: "Facebook", priority: 5)
-    ]
+    @State private var marketList: Markets = []
     
     @State private var text = ""
+//    private var data = [
+//        Company(name: "Amazon", priority: 0),
+//        Company(name: "Apple", priority: 1),
+//        Company(name: "Google", priority: 2),
+//        Company(name: "Tesla", priority: 3),
+//        Company(name: "X", priority: 4),
+//        Company(name: "Facebook", priority: 5)
+//    ]
+//    @State private var text = ""
     
-    private var filteredList: [Company] {
-        return text.isEmpty ? data : data.filter {
-            $0.name.lowercased().contains(text.lowercased()) }
+//    private var filteredList: [Company] {
+//        return text.isEmpty ? data : data.filter {
+//            $0.name.lowercased().contains(text.lowercased()) }
+//    }
+
+    private var newList: Markets {
+        return text.isEmpty ? marketList : marketList.filter { $0.koreanName.contains(text)}
     }
     
+    
     var body: some View {
-        //        TextField("회사명", text: $text)
-        //            .foregroundStyle(.black)
-        //            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        //            .padding()
         
         NavigationView {
-            List {
-                ForEach(filteredList, id: \.self.priority) { item in
-                    Text("\(item.name)")
-                        .font(.largeTitle)
+            ScrollView {
+                
+                LazyVStack(alignment: .leading) {
+                    
+                    ForEach(newList, id: \.self) { item in
+                        
+                        Text(item.koreanName)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    }
                 }
+                .padding()
+                .searchable(text: $text, placement: .navigationBarDrawer, prompt: "검색검색")
             }
-            .searchable(text: $text, placement: .navigationBarDrawer, prompt: "회사 검색 ㄱ")
+            .task {
+                do {
+                    let result = try await UpbitAPI.fetchMArket()
+                    marketList = result
+                } catch {
+                    
+                }
+        }
         }
     }
 }

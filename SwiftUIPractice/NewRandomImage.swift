5007705dec1b83c77d02e2937f954b613df6a900
dsungc1111 <eotjd9808@naver.com>
2024-09-05 17:21:40 +0900

    - 섹션 타이틀 수정 후, 메인화면에 반영되도록 코드 수정

--- SwiftUIPractice/NewRandomImage.swift
+++ SwiftUIPractice/NewRandomImage.swift
@@ -7,45 +7,89 @@
 
 import SwiftUI
 
+struct SectionModel: Hashable, Identifiable {
+    var title: String
+    let id = UUID()
+}
+
 struct NewRandomImage: View {
+
     
+    @State private var sectionTitle = [
+        SectionModel(title: "첫번째"),
+        SectionModel(title: "두번째"),
+        SectionModel(title: "세번째"),
+        SectionModel(title: "네번째")
     
-    let title = [
-        "첫번째",
-        "두번째",
-        "세번째",
-        "네번째"
     ]
-   
+    
+    private var navigtaionTitle = "MY Random Image"
+    
     
     var body: some View {
+        NavigationView {
+            ScrollView(.vertical) {
+                ForEach($sectionTitle, id: \.id) { item in
+                    sectionTitleView(item.title)
+                    imageView(item.title)
+                }
+            }
+            .scrollIndicators(.hidden)
+        }
+    }
+    
+    func sectionTitleView(_ item: Binding<String>) -> some View {
         
+        Text(item.wrappedValue)
+            .font(.title2).bold()
+            .padding(.leading)
+            .navigationTitle(navigtaionTitle)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+    }
+    
+    func imageView(_ text: Binding<String>) -> some View {
         
-        ScrollView(.vertical) {
-            ForEach(title, id: \.self) { item in
-                Text(item)
-                    .font(.headline)
-                    .padding(.leading)
-                ScrollView(.horizontal) {
-                    VStack {
-                        HStack {
-                            ForEach(0..<10) {_ in
-                                var number = Int.random(in: 0...100)
-                                let url = URL(string: "https://picsum.photos/id/\(number)/150/200")
-                                
-                                AsyncImage(url: url)
-                                    .clipShape(.buttonBorder)
-                            }
+        ScrollView(.horizontal) {
+            VStack {
+                HStack {
+                    ForEach(0..<10) {_ in
+                        let number = Int.random(in: 0...100)
+                        let url = URL(string: "https://picsum.photos/id/\(number)/130/180")
+                        
+                        NavigationLink {
+                            SecondView(url: url, text: text)
+                        } label: {
+                            AsyncImage(url: url)
+                                .clipShape(.buttonBorder)
                         }
                     }
                 }
-                
-                .scrollIndicators(.hidden)
             }
-           
-          
+            .padding()
         }
         .scrollIndicators(.hidden)
+    }
+    
+}
+
+
+
+
+
+struct SecondView: View {
+    
+    let url: URL?
+    
+    @Binding var text: String
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: url)
+                .clipShape(.buttonBorder)
+                .frame(width: 200, height: 300)
+            TextField("검색하슈", text: $text)
+                .padding()
+        }
     }
 }
 
